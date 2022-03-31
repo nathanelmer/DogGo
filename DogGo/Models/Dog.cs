2f0588ef35bea39e9b223f36808d58841e88194b
Nathan Elmer <nelmer12@gmail.com>
2022-03-31 14:06:48 -0500

    finished chapter 7

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,12 +1,25 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(45, MinimumLength = 5)]
         public string Name { get; set; }
+
+        [Required]
         public int OwnerId { get; set; }
+
+        [Required]
         public string Breed { get; set; }
+
+        [Display(Name = "Notes (Optional)")]
         public string Notes { get; set; }
+
+        [Display(Name = "Image Url (Optional)")]
         public string ImageUrl { get; set; }
         public Owner Owner { get; set; }
     }
--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,9 +1,16 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(45, MinimumLength = 5)]
         public string Name { get; set; }
+
+        [Required]
         public int NeighborhoodId { get; set; }
         public string ImageUrl { get; set; }
         public Neighborhood Neighborhood { get; set; }
